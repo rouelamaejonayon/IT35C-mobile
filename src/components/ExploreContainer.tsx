d1e45d50d038bc488349f04d32105cca72c105e4
rouelamaejonayon <jonayonrouelamae@gmail.com>
2026-03-30 15:32:39 +0800

    Login

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -6,19 +6,11 @@ interface ContainerProps { }
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
-      <IonCard>
-            <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
-            <IonCardHeader>
-              <IonCardTitle>Hello World</IonCardTitle>
-              <IonCardSubtitle>It35c</IonCardSubtitle>
-            </IonCardHeader>
-      
-            <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
-          </IonCard>
+
     </div>
   );
-  
-  
+
+
 };
 
 export default ExploreContainer;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,28 @@
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonRouter } from "@ionic/react";
+
+const Login: React.FC = () => {
+    const navigation = useIonRouter();
+
+    const doLogin = () => {
+        navigation.push('/app', 'forward', 'replace')
+    }
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Login</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+                <IonButton expand="full">
+                    Login
+                </IonButton>
+
+            </IonContent>
+        </IonPage>
+
+    );
+};
+
+export default Login;
